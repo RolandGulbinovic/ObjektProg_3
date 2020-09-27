3e452ee386cf80d83ae6f81fb018387de925a12f
RolandGulbinovic <71870974+RolandGulbinovic@users.noreply.github.com>
2020-09-27 18:08:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,12 +17,12 @@ using std::setprecision;
 using std::fixed;
 
 struct studentas {
-	string Vardas[10];
-	string Pav[10];
-	int nd[10][15];
-	int egz[10];
-	float GP[10];
-	float Med[10];
+	string Vardas;
+	string Pav;
+	int nd[15];
+	int egz;
+	float GP;
+	float Med;
 };
 
 
@@ -30,62 +30,67 @@ struct studentas {
 int main()
 {
 
-	studentas Eil; studentas Eil_mas[5];
+	studentas Eil_mas[5];
 	vector<studentas> Eiles;
 	int n;
 	int sk;
-	int l;
+	int l=0;
 	cout << "Norite gauti studentu mediana(1) ar Galutini pazymi(2)" << endl;
 	cin >> sk;
-
-	for (int i = 0; i < 10; i++) {
-		Eil.GP[i] = 0;
-		Eil.Med[i] = 0;
-	}
 
 	
 	cout << "Iveskite studentu skaiciu" << endl;
 	cin >> l;
-	for (int i = 1; i <= l; i++) {
 
-		cout << "Iveskite "<< i <<"-ojo studento varda ir pavarde" << endl;
-		cin >> Eil.Vardas[i] >> Eil.Pav[i];
+	studentas* Eil = new studentas[l];
+	
+
+	for (int i = 0; i < l; i++) {
+		Eil[i].GP = 0;
+		Eil[i].Med = 0;
+	}
+
+	for (int i = 0; i < l; i++) {
+
+		cout << "Iveskite "<< i+1 <<"-ojo studento varda ir pavarde" << endl;
+		cin >> Eil[i].Vardas >> Eil[i].Pav;
 		cout << "Iveskite namu darbu skaiciu" << endl;
 		cin >> n;
 		cout << "Prasome ivesti visus namu darbu pazymius" << endl;
 		for (int j = 0; j < n; j++)
 		{
-			cin >> Eil.nd[i][j];
-			Eil.GP[i] = Eil.nd[i][j] + Eil.GP[i];
+			cin >> Eil[i].nd[j];
+			Eil[i].GP = Eil[i].nd[j] + Eil[i].GP;
 		}
 		cout << "Iveskite egzamino rez." << endl;
-		cin >> Eil.egz[i];
+		cin >> Eil[i].egz;
 
 		if (sk == 2)
 		{
-			Eil.GP[i] = Eil.GP[i] / (float)n;
-			Eil.GP[i] = 0.4 * Eil.GP[i] + 0.6 * (float)Eil.egz[i]; //GP skaiciavumas
+			Eil[i].GP = Eil[i].GP / (float)n;
+			Eil[i].GP = 0.4 * Eil[i].GP + 0.6 * (float)Eil[i].egz; //GP skaiciavumas
 		}
 		else {
-			Eil.nd[i][n] = Eil.egz[i];
-			sort(Eil.nd[i], Eil.nd[i] + (n + 1));
+			Eil[i].nd[n] = Eil[i].egz;
+			sort(Eil[i].nd, Eil[i].nd + (n + 1));
 			if (n + 1 % 2 != 0)
-				Eil.Med[i] = (float)Eil.nd[i][(n + 1) / 2];
+				Eil[i].Med = (float)Eil[i].nd[(n + 1) / 2];
 
-			Eil.Med[i] = (float)(Eil.nd[i][n / 2] + Eil.nd[i][(n + 1) / 2]) / 2.0;
+			Eil[i].Med = (float)(Eil[i].nd[n / 2] + Eil[i].nd[(n + 1) / 2]) / 2.0;
 		}
+
 	}
 
-	for(int i=1;i<=l;i++){
+	for(int i=0;i<l;i++){
 		if (sk == 2)
 		{
 			cout << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis Paz"<< endl;
-			cout << Eil.Vardas[i] << setw(15) << Eil.Pav[i] << setw(15) << fixed << setprecision(2) << Eil.GP[i]; ; //Isvedimas
+			cout << Eil[i].Vardas << setw(15) << Eil[i].Pav << setw(15) << fixed << setprecision(2) << Eil[i].GP; ; //Isvedimas
 			cout << endl;
 		}
 		else {
 			cout << "Vardas" << setw(15) << "Pavarde" << setw(10) << "Galutinis Paz"<< setw(20) << endl;
-			cout << Eil.Vardas[i] << setw(15) << Eil.Pav[i] << setw(15) << fixed << setprecision(2) << Eil.Med[i]; //Isvedimas
+			cout << Eil[i].Vardas << setw(15) << Eil[i].Pav << setw(15) << fixed << setprecision(2) << Eil[i].Med; //Isvedimas
 			cout << endl;
 		}
 	}
