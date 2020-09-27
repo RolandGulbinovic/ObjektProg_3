cb0aef7fdbf48e1788872472c1517ab79061c0fb
RolandGulbinovic <71870974+RolandGulbinovic@users.noreply.github.com>
2020-09-27 13:03:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,36 +2,45 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <functional>
+#include <array>
+
 
 using std::cout;
 using std::endl;
 using std::string;
 using std::cin;
 using std::vector;
+using std::sort;
 
-struct studentas{
+struct studentas {
 	string Vardas[10];
 	string Pav[10];
 	int nd[10][15];
 	int egz[10];
-	int sk[10];
 	float GP[10];
+	float Med[10];
 };
 
-void insertionSort(int arr[], int n);
-void printArray(int arr[], int n);
-double findMedian(int a[], int n);
+
 
 int main()
 {
+
 	studentas Eil; studentas Eil_mas[5];
 	vector<studentas> Eiles;
-
-	for (int i = 0; i < 10; i++) Eil.GP[i] = 0;
-
 	int n;
 	int sk;
 	int l;
+	cout << "norite medianas(1) ar GP(2)" << endl;
+	cin >> sk;
+
+	for (int i = 0; i < 10; i++) {
+		Eil.GP[i] = 0;
+		Eil.Med[i] = 0;
+	}
+
+	
 	cout << "Iveskite studentu skaiciu" << endl;
 	cin >> l;
 	for (int i = 0; i < l; i++) {
@@ -48,61 +57,34 @@ int main()
 		cout << "Iveskite egzamino rez." << endl;
 		cin >> Eil.egz[i];
 
-		cout << "norite medianas(1) ar GP(2)" << endl;
-		cin >> sk;
 		if (sk == 2)
 		{
 			Eil.GP[i] = Eil.GP[i] / (float)n;
 			Eil.GP[i] = 0.4 * Eil.GP[i] + 0.6 * (float)Eil.egz[i]; //GP skaiciavumas
 		}
 		else {
-			/*Eil.nd[n] = Eil.egz;
+			Eil.nd[i][n] = Eil.egz[i];
+			sort(Eil.nd[i], Eil.nd[i] + (n + 1));
+			if (n + 1 % 2 != 0)
+				Eil.Med[i] = (float)Eil.nd[i][(n + 1) / 2];
 
-			insertionSort(Eil.nd , n+1);
-			cout << Eil.Vardas << " " << Eil.Pav <<" "<< findMedian(Eil.nd, n + 1) <<endl;
-			*/
+			Eil.Med[i] = (float)(Eil.nd[i][n / 2] + Eil.nd[i][(n + 1) / 2]) / 2.0;
 		}
 	}
-	for (int i = 0; i < l; i++) {
-		cout << Eil.Vardas[i] << " " << Eil.Pav[i] << " "; //Isvedimas
-		printf("%.2f", Eil.GP[i]);
-		cout << endl;
-	}
-}
 
-/* Function to sort an array using insertion sort*/
-/*void insertionSort(int arr[], int n)
-{
-	int i, key, j;
-	for (i = 1; i < n; i++)
-	{
-		key = arr[i];
-		j = i - 1;
 
-		/* Move elements of arr[0..i-1], that are
-		greater than key, to one position ahead
-		of their current position 
-		while (j >= 0 && arr[j] > key)
+
+	for(int i=0;i<l;i++){
+		if (sk == 2)
 		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
+			cout << Eil.Vardas[i] << " " << Eil.Pav[i] << " "; //Isvedimas
+			printf("%.2f", Eil.GP[i]);
+			cout << endl;
 		}
-		arr[j + 1] = key;
+		else {
+			cout << Eil.Vardas[i] << " " << Eil.Pav[i] << " "; //Isvedimas
+			printf("%.2f", Eil.Med[i]);
+			cout << endl;
+		}
 	}
 }
-
-// A utility function to print an array of size n  
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-double findMedian(int a[], int n)
-{
-	if (n % 2 != 0)
-		return (double)a[n / 2];
-
-	return (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0;
-}*/
