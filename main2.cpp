8830421713066615012ef3363b12378102bc8d52
RolandGulbinovic <71870974+RolandGulbinovic@users.noreply.github.com>
2020-09-30 13:44:11 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -20,7 +20,7 @@ using std::rand;
 struct studentas {
 	string Vardas;
 	string Pav;
-	int nd[15];
+	int nd;
 	int egz;
 	float GP;
 	float Med;
@@ -31,8 +31,6 @@ struct studentas {
 int main()
 {
 	vector<studentas> Eiles;
-	studentas laik;
-	int n;
 	int sk;
 	int l = 0;
 	int t;
@@ -43,30 +41,56 @@ int main()
 	cout << "Iveskite studentu skaiciu" << endl;
 	cin >> l;
 	Eiles.reserve(l);
+	
 
 	for (int i = 0; i < l; i++) {
+		int n = 0;
+		studentas laik;
+		vector<int> nd;
+		nd.reserve(n);
 
 		laik.GP = 0;
 		laik.Med = 0;
 
 		cout << "Iveskite " << i + 1 << "-ojo studento varda ir pavarde" << endl;
 		cin >> laik.Vardas >> laik.Pav;
-		cout << "Iveskite namu darbu skaiciu" << endl;
-		cin >> n;
 		cout << "Uzpildyti automatiskai? (random) 1- Taip/2 - NE" << endl;
 		cin >> t;
 		if (t == 1) {
+			cout << "kiek skaiciu generuoti" << endl;
+			cin >> n;
 			for (int j = 0; j < n; j++) {
-				laik.nd[j] = rand() % 11;
-				laik.GP = laik.nd[j] + laik.GP;
+
+				laik.nd = rand() % 11;
+
+				laik.GP = laik.nd + laik.GP;
+				nd.push_back(rand() % 11);
+
 			}
 		}
 		else {
-			cout << "Prasome ivesti visus namu darbu pazymius" << endl;
-			for (int j = 0; j < n; j++)
-			{
-				cin >> laik.nd[j];
-				laik.GP = laik.nd[j] + laik.GP;
+			cout << "Prasome ivesti visus namu darbu pazymius kai uzbaigete pildyti parasykite (-1)" << endl;
+
+			while (true)
+			{	
+				
+				cin >> laik.nd;
+				if (laik.nd > 0) {
+					
+					n++;
+
+					nd.reserve(n);
+					laik.GP = laik.nd + laik.GP;
+					cout << laik.GP << endl;
+					nd.push_back(laik.nd);
+
+				}
+				else if (laik.nd == -1) {
+					cin.clear();
+					cin.ignore(10, '\n');
+					break;
+				}
+
 			}
 		}
 		
@@ -79,18 +103,22 @@ int main()
 			laik.GP = 0.4 * laik.GP + 0.6 * (float)laik.egz; //GP skaiciavumas
 		}
 		else {
-			laik.nd[n] = laik.egz;
-			sort(laik.nd, laik.nd + (n + 1));
-			if (n + 1 % 2 != 0)
-				laik.Med = (float)laik.nd[(n + 1) / 2];
 
-			laik.Med = (float)(laik.nd[n / 2] + laik.nd[(n + 1) / 2]) / 2.0;
+			nd.reserve(n + 1);
+
+			nd[nd.size()-1] = laik.egz;
+
+			sort(nd.begin(), nd.end());
+
+			if (nd.size() % 2 != 0)
+				laik.Med = (float)nd[(nd.size()) / 2];
+
+			laik.Med = (float)(nd[nd.size() / 2] + nd[(nd.size() + 1) / 2]) / (float)2.0;
 		}
 		Eiles.push_back(laik);
-
 	}
 
-	for (auto tt: Eiles) {
+	for (auto tt : Eiles) {
 		if (sk == 2)
 		{
 			cout << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis Paz" << endl;
@@ -103,4 +131,5 @@ int main()
 			cout << endl;
 		}
 	}
+	Eiles.clear();
 }
