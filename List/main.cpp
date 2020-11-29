c5fb27ddff8de3fdd4c21a1c30397c218458b6b5
RolandGulbinovic <71870974+RolandGulbinovic@users.noreply.github.com>
2020-11-29 15:45:23 +0200

    Add files via upload

---
+++ List/main.cpp
@@ -0,0 +1,37 @@
+#include "mybib.h"
+#include "mybib2.h"
+int main()
+{
+	int k;
+	int studentai;
+	int sk = 0;
+	list<studentas> Eiles, blogi_paz, geri_paz;
+
+	cout << "Norite gauti studentu mediana(1) ar Galutini pazymi(2)" << endl;
+
+	while (!(cin >> sk) || (sk != 1 && sk != 2))
+	{
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "ERORR: Ivesti galima tik 1 arba 2" << endl;
+
+	}
+
+
+	for (int i = 0; i < 5; i++) {
+		cout << "--------------------------" << endl;
+		cout << "kiek norite eiluciu faile?" << endl;
+		cin >> k;
+
+		KurtiFaila(5, k);
+		nuskaitymas(k, sk, Eiles, 5);
+		rusiavimas(Eiles, geri_paz, blogi_paz, k);
+		isvedimas(Eiles, geri_paz, blogi_paz, k);
+
+		Eiles.clear();
+		blogi_paz.clear();
+		geri_paz.clear();
+	}
+
+
+}
---
+++ List/mybib2.h
@@ -0,0 +1,122 @@
+﻿#include "mybib.h"
+
+void KurtiFaila(int, int);
+void nuskaitymas(int, int, list<studentas>&, int);
+void isvedimas(list<studentas>&, list<studentas>&, list<studentas>&, int);
+void rusiavimas(list<studentas>&, list<studentas>&, list<studentas>&, int);
+
+void KurtiFaila(int nd, int studentai) {
+
+	string  ss = to_string(studentai);
+	string fileName = "file" + ss;
+
+	ofstream file;
+	file.open(fileName + ".txt");
+	for (int i = 0; i < studentai; i++) {
+		file << "Vardas" + to_string(i) << " " << "Pavarde" + to_string(i);
+		for (int i = 0; i < nd; i++)
+			file << " " << rand() % 11;
+		file << " " << rand() % 11;
+		file << "\n";
+	}
+	file.close();
+
+}
+
+
+void nuskaitymas(int studentai, int sk, list<studentas>& Eiles, int n)
+{
+	auto start = high_resolution_clock::now();
+	ifstream file;
+	list<int> ndf;
+
+	string  ss = to_string(studentai);
+	string fileName = "file" + ss;
+
+	file.open("C:/Users/rolan/Desktop/0.4.1/" + fileName + ".txt");
+
+	studentas laik;
+
+	while (file >> laik.Vardas >> laik.Pav)
+	{
+		laik.GP = 0;
+		laik.Med = 0;
+		for (int i = 0; i < n; i++) {
+			file >> laik.nd;
+			laik.GP = laik.nd + laik.GP;
+			ndf.push_back(laik.nd);
+		}
+		file >> laik.egz;
+
+		if (sk == 2) {
+			if (n == 0)
+				laik.GP = 0; //jei n=0 tai kad nedalintu is 0
+			else {
+				laik.GP = laik.GP / (float)n;
+				laik.GP = 0.4 * laik.GP + 0.6 * (float)laik.egz; //GP skaiciavumas
+			}
+		}
+		else {
+			laik.GP = 0;
+			int c = ndf.size();
+			ndf.push_front(laik.egz);
+
+			ndf.sort();
+			list<int>::iterator it = ndf.begin();
+			std::advance(it, ((ndf.size() - 1) / 2));
+
+
+			if (ndf.size() % 2 != 0)
+				laik.GP = (float)*it;
+
+			laik.GP = ((float)*it + (float)*it++) / (float)2.0;
+
+		}
+
+		Eiles.push_back(laik);
+		ndf.clear();
+	}
+
+	duration<double> elapsed_seconds = high_resolution_clock::now() - start;
+	cout << "Skaitymas nuo failo uztruko: " << elapsed_seconds.count() << "s." << endl;
+}
+
+void rusiavimas(list<studentas>& Eiles, list<studentas>& geri_paz, int studentai)
+{
+	auto start = high_resolution_clock::now();
+
+
+	auto result = std::remove_if(Eiles.begin(), Eiles.end(), [](const studentas& p)
+		{
+			return p.GP < 5;
+		});
+
+	geri_paz.insert(geri_paz.begin(), result, Eiles.end());
+
+	Eiles.erase(result, Eiles.end());
+
+
+	duration<double> elapsed_seconds = high_resolution_clock::now() - start;
+	cout << "Rusiavimas i 2 vektorius uztruko: " << elapsed_seconds.count() << "s." << endl;
+}
+
+void isvedimas(list<studentas>& Eiles, list<studentas>& geri_paz, int studentai) {
+	auto start = high_resolution_clock::now();
+	ofstream blogas, geras;
+	blogas.open("blogas_paz.txt");
+	for (auto tt : geri_paz) { //isvedu du naujus vektorius i failus
+		blogas << tt.Vardas << " " << tt.Pav << " " << fixed << setprecision(2) << tt.GP;
+		blogas << '\n';
+	}
+	blogas.close();
+
+	geras.open("geras_paz.txt");
+	for (auto tt : Eiles) { //isvedu du naujus vektorius i failus
+		geras << tt.Vardas << " " << tt.Pav << " " << fixed << setprecision(2) << tt.GP;
+		geras << '\n';
+	}
+	geras.close();
+	duration<double> elapsed_seconds = high_resolution_clock::now() - start;
+	cout << "Isvedimas i failus uztruko: " << elapsed_seconds.count() << "s" << endl;
+
+}
